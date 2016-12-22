1c198585fc1719c803f652f68ea5e419e6113718
Mark <mr@willis-organs.com>
2016-12-22 11:10:10 +0000

    Complete Reverse relationship lesson

--- routes/web.php
+++ routes/web.php
@@ -14,21 +14,12 @@ use App\PhoneNumber;
 use Illuminate\Http\Request;
 
 Route::get('/', function (Request $request) {
-    // return view('welcome');
-    // dd($request->user());
-    // access user and phone number relation
-    // no need to access user_id as Eloquent knows
-    // $request->user()->phoneNumber()->create([
-    //     'phone_number' => '0123456789',
-    // ]);
-    // dd($request->user()->phoneNumber()->get());
-    // $phoneNumber = PhoneNumber::find(1);
+    
+    $phoneNumbers = PhoneNumber::get();
 
-    $phoneNumber = new PhoneNumber;
-
-    $phoneNumber->phone_number = '0123456789';
-    $phoneNumber->user()->associate($request->user());
-    $phoneNumber->save();
+    return view('phone.index', compact('phoneNumbers')
+        // ['phoneNumbers' => $phoneNumbers] SAME AS COMPACT
+    );
 });
 
 Auth::routes();
